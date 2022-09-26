fb3171659b8d8de8611d91bc02956f5b1d4abd37
vinicius silva <vinicius_dev@hotmail.com>
2022-09-26 03:37:50 -0300

    feat: Implementa cors

--- src/classes/Routes.ts
+++ src/classes/Routes.ts
@@ -5,15 +5,17 @@ class Routes {
   router: Router
   controller: Controller
   path: string
+  cors: any
 
-  constructor(controller: Controller, path: string) {
+  constructor(controller: Controller, path: string, cors: any) {
     this.router = Router()
     this.controller = controller
     this.path = path
+    this.cors = cors
   }
 
   public addRoute() {
-    this.router.get(`/${this.path}`, this.controller.readAll)
+    this.router.get(`/${this.path}`, this.cors(), this.controller.readAll)
     this.router.post(`/${this.path}`, this.controller.create)
     this.router.put(`/${this.path}/:id`, this.controller.update)
     this.router.delete(`/${this.path}/:id`, this.controller.delete)
