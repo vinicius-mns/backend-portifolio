afc542afa06fdb0a61ffae1c12e332c19a29778e
vinicius silva <vinicius_dev@hotmail.com>
2022-09-26 03:38:43 -0300

    feat: Adiciona cors ao document projects

--- src/documents/projects/index.ts
+++ src/documents/projects/index.ts
@@ -2,8 +2,9 @@ import projectsColletion from "../../connection/mongodb"
 import Controller from "../../classes/Controller";
 import MyRouter from '../../classes/Routes';
 import ProjectSchema from './Schema';
+const cors = require('cors')
 
 const project = new Controller(projectsColletion, ProjectSchema)
-const routeProject = new MyRouter(project, 'project')
+const routeProject = new MyRouter(project, 'project', cors)
 
 export default routeProject
