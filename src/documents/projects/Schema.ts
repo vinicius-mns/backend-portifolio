b3e9cc1b92404e7cfafd9578e9d611e8f5860283
vinicius silva <vinicius_dev@hotmail.com>
2022-09-26 02:39:06 -0300

    feat: Cria Schema para projetos

---
+++ src/documents/projects/Schema.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+const img_alt = z.object({
+  img: z.string(),
+  alt: z.string()
+})
+
+const ProjectSchema = z.object({
+  image: img_alt,
+  title: z.string(),
+  description: z.string().array(),
+  github: z.string(),
+  site: z.string(),
+  stacks: img_alt.array()
+})
+
+export default ProjectSchema
