c16ff3a5fdd80f49e5fca1b0cf3cb7f8a0b571ac
vinicius silva <vinicius_dev@hotmail.com>
2022-09-29 06:02:23 -0300

    refacor: altera schema projects

--- src/documents/projects/Schema.ts
+++ src/documents/projects/Schema.ts
@@ -1,16 +1,16 @@
 import { z } from 'zod'
 
 const img_alt = z.object({
-  img: z.string(),
-  alt: z.string()
+  image: z.string(),
+  description: z.string()
 })
 
 const ProjectSchema = z.object({
-  // image: img_alt,
+  tamb: img_alt,
   title: z.string(),
-  // description: z.string(),
-  // github: z.string(),
-  // site: z.string(),
+  description: z.string(),
+  repository: z.string(),
+  site: z.string(),
   // stacks: img_alt.array()
 })
 
