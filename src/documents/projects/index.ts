21289c1f83a69e3db7d95f0d2e6609c791acbfe3
vinicius silva <vinicius_dev@hotmail.com>
2022-10-02 12:45:24 -0300

    refactor: Corrige imporcao da collecao -> projetos (projects)

--- src/documents/projects/index.ts
+++ src/documents/projects/index.ts
@@ -1,9 +1,9 @@
-import projectsColletion from "../../connection/mongodb"
+import { projectsCollection } from "../../connection/mongodb"
 import Controller from "../../classes/Controller";
 import MyRouter from '../../classes/Routes';
 import ProjectSchema from './Schema';
 
-const project = new Controller(projectsColletion, ProjectSchema)
+const project = new Controller(projectsCollection, ProjectSchema)
 const routeProject = new MyRouter(project, 'project')
 
 export default routeProject
