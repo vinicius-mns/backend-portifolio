5b4828f11767228936c8523d2043797c4ce68a66
vinicius silva <vinicius_dev@hotmail.com>
2022-10-02 12:43:41 -0300

    feat: Implementa ao app -> quem sou eu (iam)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import App from "./app";
+import routeIam from "./documents/iam";
 import routeProject from "./documents/projects";
 
 const server = new App()
 
 routeProject.addRoute()
+routeIam.addRoute()
 
 server.addRouter(routeProject.router)
+server.addRouter(routeIam.router)
 
 server.startServer()
