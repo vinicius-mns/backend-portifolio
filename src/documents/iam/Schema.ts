ffc45af89e1babd42f9f69cd31aeba8aa3a555e5
vinicius silva <vinicius_dev@hotmail.com>
2022-10-02 12:41:29 -0300

    feat: Cria Schema para colecao -> quem sou eu (iam)

---
+++ src/documents/iam/Schema.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+
+const img_alt = z.object({
+  src: z.string(),
+  alt: z.string()
+})
+
+const IamSchema = z.object({
+  image: img_alt,
+  myName: z.string(),
+  content: z.string().array(),
+})
+
+export default IamSchema
