b46fa2c68ad8eb4ece10a8cb104d1c2b5975828f
vinicius silva <vinicius_dev@hotmail.com>
2022-10-03 06:17:08 -0300

    refacor: Alterando content de para uma string

--- src/documents/iam/Schema.ts
+++ src/documents/iam/Schema.ts
@@ -8,7 +8,7 @@ const img_alt = z.object({
 const IamSchema = z.object({
   image: img_alt,
   myName: z.string(),
-  content: z.string().array(),
+  content: z.string(),
 })
 
 export default IamSchema
