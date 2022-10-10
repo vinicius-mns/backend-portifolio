6522580ee40fd22f08d77732546451ccb89a7ae4
vinicius silva <vinicius_dev@hotmail.com>
2022-10-10 17:31:31 -0300

    refactor: Implementa a classe `Middle` nos documentos `iam`

--- src/documents/iam/index.ts
+++ src/documents/iam/index.ts
@@ -2,8 +2,10 @@ import Controller from "../../classes/Controller";
 import { iamCollection } from "../../connection/mongodb";
 import MyRouter from '../../classes/Routes'
 import IamSchema from "./Schema";
+import Middle from "../../classes/Middle";
 
-const iamController = new Controller(iamCollection, IamSchema)
-const routeIam = new MyRouter(iamController, 'iam')
+const iamController = new Controller(iamCollection)
+const middle = new Middle(IamSchema)
+const routeIam = new MyRouter(iamController, middle, 'iam')
 
 export default routeIam
