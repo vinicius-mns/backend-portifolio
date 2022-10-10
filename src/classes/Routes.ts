9393199a4abf6ad090effff65fdc5d9e6f454f2a
vinicius silva <vinicius_dev@hotmail.com>
2022-10-10 17:30:26 -0300

    refactor: Utiliza `Midle` na classe `Routes`

--- src/classes/Routes.ts
+++ src/classes/Routes.ts
@@ -1,22 +1,24 @@
 import { Router } from "express";
 import Controller from "./Controller";
+import Middle from "./Middle";
 
 class Routes {
   router: Router
   controller: Controller
   path: string
-  cors: any
+  middle: Middle
 
-  constructor(controller: Controller, path: string) {
+  constructor(controller: Controller, middle: Middle ,path: string) {
     this.router = Router()
     this.controller = controller
     this.path = path
+    this.middle = middle
   }
 
   public addRoute() {
     this.router.get(`/${this.path}`, this.controller.readAll)
-    this.router.post(`/${this.path}`, this.controller.create)
-    this.router.put(`/${this.path}/:id`, this.controller.update)
+    this.router.post(`/${this.path}`, this.middle.validSchema, this.controller.create)
+    this.router.put(`/${this.path}/:id`, this.middle.validSchema, this.controller.update)
     this.router.delete(`/${this.path}/:id`, this.controller.delete)
   }
 }
