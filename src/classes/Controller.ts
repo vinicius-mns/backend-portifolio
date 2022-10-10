0561c438ae55128716520ada5cb8a8aa13cb4a49
vinicius silva <vinicius_dev@hotmail.com>
2022-10-10 17:44:20 -0300

    refactor: Remove middle de dentro do controller -> single responsibility

--- src/classes/Controller.ts
+++ src/classes/Controller.ts
@@ -1,29 +1,18 @@
 import { Request, Response } from "express";
 import { Collection, ObjectId } from "mongodb";
-import { z } from "zod";
 
 class Controller {
-  private collection: Collection
-  private schema: z.ZodTypeAny
+  protected collection: Collection
 
-  constructor(collection: Collection, schema: z.ZodTypeAny) {
+  constructor(collection: Collection) {
     this.collection = collection
-    this.schema = schema
   }
 
-  create = (req: Request, res: Response) => {
+  create = async (req: Request, res: Response) => {
     try {
       const body = req.body
-      const parsed = this.schema.safeParse(body)
-
-      if (!parsed.success){
-        return res.status(400).json(parsed.error)
-      }
-
       this.collection.insertOne(body)
-
       return res.status(201).json(body)
-
     } catch (error) {
       console.log(error)
       return res.status(500).json(error)
@@ -36,30 +25,19 @@ class Controller {
   }
 
   update = async (req: Request, res: Response) => {
-    const { id } = req.params
-    const body = req.body
-
-    const parsed = this.schema.safeParse(body)
-
-    if (!parsed.success){
-      return res.status(400).json(parsed.error)
-    }
-
-    const novo = await this.collection.updateOne({_id: new ObjectId(id)}, {$set: body})
-
-    return res.status(200).json(novo)
+    try {
+      const { id } = req.params; const body = req.body
+      const novo = await this.collection.updateOne({_id: new ObjectId(id)}, {$set: body})
+      return res.status(200).json(novo)
+    } catch (err) { return res.status(404).json({message: 'Not found'}) }
   }
 
   delete = async (req: Request, res: Response) => {
     try {
       const { id } = req.params
-
-      this.collection.deleteOne({_id: new ObjectId(id)})
-  
-      return res.status(202).json({message: 'Deleted'})
-    } catch (err) {
-      return res.status(404).json({message: 'Not found'})
-    }
+      const deleted = await this.collection.deleteOne({_id: new ObjectId(id)})
+      return res.status(202).json(deleted)
+    } catch (err) { return res.status(404).json({message: 'Not found'}) }
   }
 }
 
