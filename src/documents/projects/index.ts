726100648af330ea82b8a6918d8751007da6038f
vinicius silva <vinicius_dev@hotmail.com>
2022-10-10 17:32:09 -0300

    refactor: Implementa a classe `Middle` nos documentos `projetos`

--- src/documents/projects/index.ts
+++ src/documents/projects/index.ts
@@ -2,8 +2,10 @@ import { projectsCollection } from "../../connection/mongodb"
 import Controller from "../../classes/Controller";
 import MyRouter from '../../classes/Routes';
 import ProjectSchema from './Schema';
+import Middle from "../../classes/Middle";
 
-const project = new Controller(projectsCollection, ProjectSchema)
-const routeProject = new MyRouter(project, 'project')
+const project = new Controller(projectsCollection)
+const middle = new Middle(ProjectSchema)
+const routeProject = new MyRouter(project, middle, 'project')
 
 export default routeProject
