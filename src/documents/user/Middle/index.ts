59483b3cf04ca9d30fd437a19a222e87baa89cbd
vinicius silva <vinicius_dev@hotmail.com>
2022-10-12 18:52:32 -0300

    feat: Cria middle para validar usuario existente

--- src/documents/user/Middle/index.ts
+++ src/documents/user/Middle/index.ts
@@ -1,9 +1,25 @@
 import BaseMiddle from '../../../classes/BaseMiddle'
 import Schema from '../Schema'
+import Service from '../Service'
+import { NextFunction, Request, Response } from 'express'
 
 class Middle extends BaseMiddle {
-  constructor(schema = Schema) {
+  service: typeof Service
+
+  constructor(schema = Schema, service = Service) {
     super(schema)
+    this.service = service
+  }
+
+  unicName = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name } = req.body as { name: string }
+      const user = await this.service.findUser(name)
+      if (user) {
+        return res.status(400).json({message: 'user already exist'})
+      }
+      next()
+    } catch (e) { return res.status(500).json(e) }
   }
 }
 
