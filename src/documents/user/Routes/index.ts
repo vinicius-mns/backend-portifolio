ac19773744c1e6b9215137fc5adb75e3b0a97968
vinicius silva <vinicius_dev@hotmail.com>
2022-10-12 19:04:12 -0300

    feat: Implementa middles as rotas de criacao de usuario

--- src/documents/user/Routes/index.ts
+++ src/documents/user/Routes/index.ts
@@ -3,12 +3,22 @@ import Middle from "../Middle"
 import Controller from "../Controller"
 
 class Routes extends BaseRoutes {
+  middle: typeof Middle
+
   constructor(path: string, controller = Controller, middle = Middle){
     super(controller, middle, path)
+    this.middle = middle
   }
 
   public addRoute(): void {
     this.router.get(`/${this.path}`, this.controller.readAll)
+    this.router.post(
+      `/${this.path}`,
+      this.middle.validSchema,
+      this.middle.unicName,
+      this.middle.notSpaces,
+      this.controller.create
+    )
   }
 }
 
