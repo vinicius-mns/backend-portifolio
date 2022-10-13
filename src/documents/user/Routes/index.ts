df985a56649a7603da3a6d733f43ab3d4bf4b50a
vinicius silva <vinicius_dev@hotmail.com>
2022-10-13 14:29:15 -0300

    feat: Cria rota com middles para criar usuario

--- src/documents/user/Routes/index.ts
+++ src/documents/user/Routes/index.ts
@@ -3,20 +3,26 @@ import Middle from "../Middle"
 import Controller from "../Controller"
 
 class Routes extends BaseRoutes {
-  middle: typeof Middle
+  m: typeof Middle
+  controller: typeof Controller
 
   constructor(path: string, controller = Controller, middle = Middle){
     super(controller, middle, path)
-    this.middle = middle
+    this.m = middle
+    this.controller = controller
   }
 
   public addRoute(): void {
-    this.router.get(`/${this.path}`, this.controller.readAll)
-    this.router.post(
+    this.router.get(
       `/${this.path}`,
-      this.middle.validSchema,
-      this.middle.unicName,
-      this.middle.notSpaces,
+      this.controller.readAll
+    )
+    this.router.post(
+      `/${this.path}/create`,
+      this.m.validSchema,
+      this.m.unicName,
+      this.m.notSpaces,
+      this.m.isAdm,
       this.controller.create
     )
   }
