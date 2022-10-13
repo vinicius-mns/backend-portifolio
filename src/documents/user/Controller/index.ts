5f4907ea6fe04b75aaea74fee9154fd0a16967f7
vinicius silva <vinicius_dev@hotmail.com>
2022-10-13 14:28:02 -0300

    feat: Cria controller para criar usuario

--- src/documents/user/Controller/index.ts
+++ src/documents/user/Controller/index.ts
@@ -1,32 +1,41 @@
 import BaseController from "../../../classes/BaseController"
 import Service from '../Service'
-import { Request, Response } from "express"
-import { UserType } from "../Schema"
-const jwt = require('jsonwebtoken')
+import { Request, Response } from 'express'
+import UserSchema, { User, Hash } from "../Schema"
+import { z } from "zod"
+var jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 
-class Controller extends BaseController {
+class Controller extends BaseController{
+  service: typeof Service
+
   constructor(service = Service) {
     super(service)
+    this.service = service
   }
 
   create = async (req: Request, res: Response) => {
     try {
-      const { name, password, linkedin } = req.body as UserType
+      const { name, linkedin, password, adm } = req.body as z.infer<typeof UserSchema>
 
-      const newUser = await this.service.create({
+      console.log('adm in controller', adm)
+
+      const user: User = {
         name,
         linkedin,
         token: jwt.sign({
             adm: false,
-            password: await bcrypt.hash(password, 10),
-          },
-          `${process.env.PASS}`
-        ),
-      })
+        password: await bcrypt.hash(password, 10),
+        linkedin,
+        hash: jwt.sign({name, adm}, `${process.env.PASS}`)
+      }
 
-      return res.status(201).json(newUser)
-    } catch (error) { return res.status(500).json(error) }
+      const created = await this.service.create(user)
+      return res.status(201).json(created)
+
+    } catch (e) {
+      return res.status(500).json(e)
+    }
   }
 
 }
