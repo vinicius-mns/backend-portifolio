17460b5f89f3f56b993b9995b1c7ab8351348ba8
vinicius silva <vinicius_dev@hotmail.com>
2022-10-13 15:23:48 -0300

    zero

--- src/connection/mongodb.ts
+++
@@ -1,7 +0,0 @@
-import { MongoClient } from "mongodb";
-
-const uri = `mongodb+srv://vinicius:${process.env.PASS}@cluster0.2vla7ld.mongodb.net/?retryWrites=true&w=majority`
-
-const client = new MongoClient(uri)
-
-export default client
